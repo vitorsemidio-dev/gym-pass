8499b0a4a6fb4c6da8c8cbe494f33efbaa6ef683
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-03-07 23:31:27 -0300

    feat: configuração e validação de variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
     host: "0.0.0.0",
   })
   .then(() => {
-    console.log(" Server is running on http://localhost:3333");
+    console.log(`🚀 Server is running on http://localhost:${env.PORT}`);
   });
