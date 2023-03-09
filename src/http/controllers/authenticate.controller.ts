0b0f9f5cb2c72414bc407b08097710a8a6f1f03f
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-08 22:49:21 -0300

    feat: criação rota para autenticação

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate.use-case'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials.error'
+
+export async function authenticateController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+    await authenticateUseCase.execute({ email, password })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+    throw err
+  }
+
+  return reply.status(200).send()
+}
