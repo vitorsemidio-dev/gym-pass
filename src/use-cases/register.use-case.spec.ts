58470379291a501f1e8aa12ca34cef0d9cc721f7
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-08 21:38:49 -0300

    test: registro com sucesso

--- src/use-cases/register.use-case.spec.ts
+++ src/use-cases/register.use-case.spec.ts
@@ -6,6 +6,20 @@ import { RegisterUseCase } from '@/use-cases/register.use-case'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users.repository'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const prismaUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const email = 'john_doe@test.com'
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     const prismaUsersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(prismaUsersRepository)
