c3276503cc584fa7ea25f1635e42e34649d7b7eb
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-03-11 10:43:48 -0300

    test: criação de academia

---
+++ src/use-cases/create-gym.use-case.spec.ts
@@ -0,0 +1,26 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms.repository'
+import { CreateGymUseCase } from '@/use-cases/create-gym.use-case'
+
+let prismaGymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    prismaGymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(prismaGymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Gym 1',
+      description: 'Gym 1 description',
+      latitude: 1,
+      longitude: 1,
+      phone: '123456',
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
