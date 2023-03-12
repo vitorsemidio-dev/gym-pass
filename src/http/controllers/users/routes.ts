3111b888176476740d3c7c8d01fb6e6a36b9af69
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 01:30:07 -0300

    feat: implementação do refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,12 +2,15 @@ import { FastifyInstance } from 'fastify'
 
 import { authenticateController } from '@/http/controllers/users/authenticate.controller'
 import { profileController } from '@/http/controllers/users/profile.controller'
+import { refreshController } from '@/http/controllers/users/refresh.controller'
 import { registerController } from '@/http/controllers/users/register.controller'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticateController)
   app.post('/users', registerController)
+
+  app.patch('/token/refresh', refreshController)
 
   // Authenticated
   app.get('/me', { onRequest: verifyJWT }, profileController)
