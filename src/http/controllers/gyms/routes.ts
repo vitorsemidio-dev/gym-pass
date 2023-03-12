945f435b13d41bad703577e92d5e5ef214977a58
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 00:40:24 -0300

    feat: adição controller para criação de academias

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
 
+import { createController } from '@/http/controllers/gyms/create.controller'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms', createController)
 }
