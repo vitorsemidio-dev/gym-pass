a846e47166b29e43f8f2762b16f7034ccf281e96
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 00:29:34 -0300

    test: adição teste e2e autenticação

--- src/http/controllers/authenticate.controller.spec.ts
+++ src/http/controllers/authenticate.controller.spec.ts
@@ -1,5 +1,31 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(1).toBe(1)
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
 })
