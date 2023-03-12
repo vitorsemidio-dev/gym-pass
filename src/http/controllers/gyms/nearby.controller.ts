7697376d5931b3c0293e5fc5262dae25d9ce7adb
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 00:46:32 -0300

    feat: adição controller para busca academias próximas

---
+++ src/http/controllers/gyms/nearby.controller.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories'
+
+export async function nearbyController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify'
 
 import { createController } from '@/http/controllers/gyms/create.controller'
+import { nearbyController } from '@/http/controllers/gyms/nearby.controller'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
+  app.get('/gyms/nearby', nearbyController)
   app.post('/gyms', createController)
 }
