31d859a63aeec118f61ff2b2ba52ab10ca644ad0
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 00:47:15 -0300

    feat: adição controller para busca academias pelo nome

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,11 +2,13 @@ import { FastifyInstance } from 'fastify'
 
 import { createController } from '@/http/controllers/gyms/create.controller'
 import { nearbyController } from '@/http/controllers/gyms/nearby.controller'
+import { searchController } from '@/http/controllers/gyms/search.controller'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/nearby', nearbyController)
+  app.get('/gyms/search', searchController)
   app.post('/gyms', createController)
 }
