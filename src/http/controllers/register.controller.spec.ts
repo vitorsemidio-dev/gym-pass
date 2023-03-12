876ca2a47786e621be9c23dd5543b7de1adadb4c
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-12 00:26:54 -0300

    test: adição teste e2 registro

--- src/http/controllers/register.controller.spec.ts
+++ src/http/controllers/register.controller.spec.ts
@@ -1,5 +1,23 @@
-import { expect, test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-  expect(1).toBe(1)
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
