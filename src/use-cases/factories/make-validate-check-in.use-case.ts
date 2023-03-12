4ab9a9bed40268ea52b45dbc874221cb7533384d
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-03-11 21:31:24 -0300

    refactor: exportação de fabricas em arquivo index.ts

--- src/use-cases/factories/make-validate-check-in.use-case.ts
+++ src/use-cases/factories/make-validate-check-in.use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins.repository'
 import { ValidateCheckInUseCase } from '@/use-cases/validate-check-in.use-case'
 
-export function makeSearchGymsUseCase() {
+export function makeValidateCheckInUseCase() {
   const prismaCheckInsRepository = new PrismaCheckInsRepository()
   const useCase = new ValidateCheckInUseCase(prismaCheckInsRepository)
   return useCase
